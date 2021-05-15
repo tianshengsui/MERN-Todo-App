2222d49a7616becef92400f1431c2f2a4d37322b
Sean <s7782519728@gmail.com>
2021-05-14 22:55:52 -0700

    Implement most functions in front end

--- server/server.js
+++ server/server.js
@@ -1,14 +1,16 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import cors from 'cors';
 import { Todo } from './models/todo.js';
 
 const app = express()
-const port = 3000
+const port = 8080
 
 app.use(express.json())
+app.use(cors())
 
 mongoose.connect(
-    MONGODB_URL,
+    'mongodb+srv://tianshengsui:19980210Sui123@cluster0.icrrd.mongodb.net/ReactTodo?retryWrites=true&w=majority',
  {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }
  ).then(() => {
      console.log("Database Connected!")

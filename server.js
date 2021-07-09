1029927ba0d59f06919c5f796e412d5b834ff0c8
Sean <s7782519728@gmail.com>
2021-07-08 17:32:04 -0700

    Refactor file structures

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import { Todo } from './models/todo.js';
 import dotenv  from "dotenv"
-const path = require("path");
+import path from "path"
 
 dotenv.config()
 
@@ -78,10 +78,10 @@ app.delete("/api/todos/:id", (req, res) => {
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('./client/build'));
+  app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
   });
 }
 

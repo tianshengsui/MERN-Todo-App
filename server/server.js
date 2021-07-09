f4bc16d2c826739cfdc7b2bd032137e0f8ae4619
Sean <s7782519728@gmail.com>
2021-07-08 17:08:52 -0700

    Set up deployment

--- server/server.js
+++ server/server.js
@@ -3,12 +3,11 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import { Todo } from './models/todo.js';
 import dotenv  from "dotenv"
+const path = require("path");
 
 dotenv.config()
 
 const app = express()
-const port = process.env.PORT || 5000;
-
 app.use(express.json())
 app.use(cors())
 
@@ -76,6 +75,17 @@ app.delete("/api/todos/:id", (req, res) => {
     });
   });
 
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('./client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
+  });
+}
+
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`App is listening at http://localhost:${port}`)
 })
